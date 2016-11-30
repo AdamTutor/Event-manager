786f1273f6e0589012e1b0968d54c9e03b488b7b
Adam Tutor <atutor@basecampcodingacademy.org>
2016-11-30 15:27:46 -0600

    init file now works properly. Added requirements.txt. adjusted sql file.

--- init.sql
+++ init.sql
@@ -1,19 +1,17 @@
-CREATE DATABASE schedule;
-
-CREATE TABLE schedule.teams (
-    "id" integer DEFAULT nextval('teams_id_seq'::regclass),
+CREATE TABLE teams (
+    "id" serial,
     "name" text NOT NULL,
     PRIMARY KEY ("id"),
     UNIQUE ("name")
 );
 
-CREATE TABLE schedule.events (
-    "id" integer DEFAULT nextval('events_id_seq'::regclass),
+CREATE TABLE events (
+    "id" serial,
     "team1" integer NOT NULL,
     "team2" integer NOT NULL,
     "datetime" timestamp without time zone NOT NULL,
     "type" text NOT NULL,
     PRIMARY KEY ("id"),
-    CONSTRAINT "events_team1_fkey" FOREIGN KEY ("team1") REFERENCES "".""(),
-    CONSTRAINT "events_team2_fkey" FOREIGN KEY ("team2") REFERENCES "".""()
+    CONSTRAINT "events_team1_fkey" FOREIGN KEY ("team1") REFERENCES teams(id),
+    CONSTRAINT "events_team2_fkey" FOREIGN KEY ("team2") REFERENCES teams(id)
 );
